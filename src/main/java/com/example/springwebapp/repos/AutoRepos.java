2a8b189b34c3dff53496e99cec03d4866990ae63
DashaPyshnenko <71897811+DashaPyshnenko@users.noreply.github.com>
2021-05-13 11:37:26 +0500

    Delete src directory

--- src/main/java/com/example/springwebapp/repos/AutoRepos.java
+++
@@ -1,7 +0,0 @@
-package com.example.springwebapp.repos;
-
-import com.example.springwebapp.model.Auto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AutoRepos extends CrudRepository<Auto,Long> {
-}
