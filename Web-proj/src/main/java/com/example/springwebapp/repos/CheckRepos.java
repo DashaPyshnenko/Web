82dbc61e86ae32a140d87fb407b54c45ac561b2e
DashaPyshnenko <71897811+DashaPyshnenko@users.noreply.github.com>
2021-05-13 11:37:13 +0500

    Delete Web-proj directory

--- Web-proj/src/main/java/com/example/springwebapp/repos/CheckRepos.java
+++
@@ -1,8 +0,0 @@
-package com.example.springwebapp.repos;
-
-import com.example.springwebapp.model.Checkout;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CheckRepos extends CrudRepository<Checkout,Long> {
-
-}
